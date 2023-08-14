bc49a4fb773feecf2c5cd2b9cf01a4ced8221e1b
unknown <cs.usama.aslam@gmail.com>
2023-08-14 23:45:40 +0500

    Remove unnecessary files

---
+++ build/test-results/timeline/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"4283d2a5-174a-4a10-b1d0-86533c824084","feature":"Search by keyword","scenario":"Searching for 'green'","start":1692038527467,"group":19,"content":"","tags":"@green,","end":1692038540959,"className":"passed"},{"id":"52a4b6b0-c83d-4b5b-a465-c39bb6713330","feature":"Search by keyword","scenario":"Searching for 'red'","start":1692038527398,"group":18,"content":"","tags":"@red,","end":1692038540959,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":18,"content":"Thread[ForkJoinPool-2-worker-1,5,main]"},{"id":19,"content":"Thread[ForkJoinPool-2-worker-2,5,main]"}]);
+});
